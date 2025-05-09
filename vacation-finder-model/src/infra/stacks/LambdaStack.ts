44159f7e477db07c7ccca8091b8388ff1a665261
jakefleming13 <jakefleming@shaw.ca>
2025-05-09 17:12:42 -0600

    Add Outputs to DataStack to be used within LamdaStack

--- vacation-finder-model/src/infra/stacks/LambdaStack.ts
+++ vacation-finder-model/src/infra/stacks/LambdaStack.ts
@@ -1,5 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
+import { ITable, Table } from "aws-cdk-lib/aws-dynamodb";
 import {
   Code,
   Function as LambdaFunction,
@@ -8,16 +9,21 @@ import {
 import { Construct } from "constructs";
 import { join } from "path";
 
+interface LambdaStackProps extends StackProps {
+  vacationsTable: ITable;
+}
+
 export class LambdaStack extends Stack {
   public readonly testLambdaIntegration: LambdaIntegration;
 
-  constructor(scope: Construct, id: string, props?: StackProps) {
+  constructor(scope: Construct, id: string, props: LambdaStackProps) {
     super(scope, id, props);
 
     const testLambda = new LambdaFunction(this, "Testlambda", {
       runtime: Runtime.NODEJS_22_X,
       handler: "testLambda.main",
       code: Code.fromAsset(join(__dirname, "..", "..", "services")),
+      environment: { TABLE_NAME: props.vacationsTable.tableName },
     });
 
     this.testLambdaIntegration = new LambdaIntegration(testLambda);
