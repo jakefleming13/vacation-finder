0c1c9352b07b4f40852cd0f661a0c5b4b1ed6b71
jakefleming13 <jakefleming@shaw.ca>
2025-05-09 17:12:19 -0600

    Create DynamoDB table within DataStack

--- vacation-finder-model/src/infra/stacks/DataStack.ts
+++ vacation-finder-model/src/infra/stacks/DataStack.ts
@@ -1,8 +1,24 @@
 import { Stack, StackProps } from "aws-cdk-lib";
+import { AttributeType, ITable, Table } from "aws-cdk-lib/aws-dynamodb";
 import { Construct } from "constructs";
+import { getSuffixFromStack } from "../Utils";
 
+//Stack that will contain the Dynamodb database
 export class DataStack extends Stack {
+  public readonly vacationsTable: ITable;
+
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
+
+    const suffix = getSuffixFromStack(this);
+
+    this.vacationsTable = new Table(this, "VacationsTable", {
+      partitionKey: {
+        name: "id",
+        type: AttributeType.STRING,
+      },
+      //To make tablename unique, use the stack suffix
+      tableName: `VacationsTable-${suffix}`,
+    });
   }
 }
