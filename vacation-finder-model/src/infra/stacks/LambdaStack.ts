458eb824e36be0f124d7887e7fc18c0787e37e0e
jakefleming13 <jakefleming@shaw.ca>
2025-05-12 12:31:45 -0600

    Update Lambda to handle put requests

--- vacation-finder-model/src/infra/stacks/LambdaStack.ts
+++ vacation-finder-model/src/infra/stacks/LambdaStack.ts
@@ -1,6 +1,7 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
 import { ITable } from "aws-cdk-lib/aws-dynamodb";
+import { Effect, PolicyStatement } from "aws-cdk-lib/aws-iam";
 import { Runtime } from "aws-cdk-lib/aws-lambda";
 import { NodejsFunction } from "aws-cdk-lib/aws-lambda-nodejs";
 import { Construct } from "constructs";
@@ -22,6 +23,14 @@ export class LambdaStack extends Stack {
       entry: join(__dirname, "..", "..", "services", "vacations", "handler.ts"),
       environment: { TABLE_NAME: props.vacationsTable.tableName },
     });
+
+    vacationsLambda.addToRolePolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        resources: [props.vacationsTable.tableArn],
+        actions: ["dynamodb:PutItem"],
+      })
+    );
 
     this.vacationsLambdaIntegration = new LambdaIntegration(vacationsLambda);
   }
--- vacation-finder-model/src/services/vacations/handler.ts
+++ vacation-finder-model/src/services/vacations/handler.ts
@@ -1,8 +1,12 @@
+import { DynamoDBClient } from "@aws-sdk/client-dynamodb";
 import {
   APIGatewayProxyEvent,
   APIGatewayProxyResult,
   Context,
 } from "aws-lambda";
+import { PostVacations } from "./PostVacations";
+
+const ddbClient = new DynamoDBClient({});
 
 //Needs to return an APIGatewayProxyResult
 async function handler(
@@ -11,17 +15,26 @@ async function handler(
 ): Promise<APIGatewayProxyResult> {
   let message: string;
 
-  //Different excecution based on the events http method being used
-  switch (event.httpMethod) {
-    case "GET":
-      message = "Get method";
-      break;
-    case "POST":
-      message = "Post method";
-      break;
-    default:
-      message = "Invalid HTTP method";
-      break;
+  try {
+    //Different excecution based on the events http method being used
+    switch (event.httpMethod) {
+      case "GET":
+        message = "Get method";
+        break;
+      case "POST":
+        message = "Post method";
+        const response = PostVacations(event, ddbClient);
+        return response;
+      default:
+        message = "Invalid HTTP method";
+        break;
+    }
+  } catch (error) {
+    console.error(error);
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
   }
 
   const response: APIGatewayProxyResult = {
