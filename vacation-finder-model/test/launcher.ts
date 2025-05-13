815ba0680434353b7f7b68a697edda3d809d39ec
jakefleming13 <jakefleming@shaw.ca>
2025-05-12 18:44:02 -0600

    Add testing for the post method

--- vacation-finder-model/test/launcher.ts
+++ vacation-finder-model/test/launcher.ts
@@ -1,3 +1,14 @@
-import { handler } from "../src/services/testLambda";
+import { handler } from "../src/services/vacations/handler";
 
-handler({} as any, {} as any);
+process.env.AWS_REGION = "eu-east-2";
+process.env.TABLE_NAME = "VacationsTable-0605f1bccdbb";
+
+handler(
+  {
+    httpMethod: "POST",
+    body: JSON.stringify({
+      location: "London",
+    }),
+  } as any,
+  {} as any
+);
