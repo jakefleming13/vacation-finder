e6a5f8fca8939396b9323e26fad86604b04c4ba5
jakefleming13 <jakefleming@shaw.ca>
2025-05-28 19:45:05 -0600

    Create VacationComponent

---
+++ vacation-finder-frontend/src/components/vacations/VacationComponent.tsx
@@ -0,0 +1,30 @@
+import genericImage from "../../assets/generic-photo.jpg";
+import type { VacationEntry } from "../model/model";
+import "./VacationComponent.css";
+
+interface VacationComponentProps extends VacationEntry {
+  reserveVacation: (vacationId: string, vacationName: string) => void;
+}
+
+export default function VacationComponent(props: VacationComponentProps) {
+  function renderImage() {
+    if (props.photoUrl) {
+      return <img src={props.photoUrl} />;
+    } else {
+      return <img src={genericImage} />;
+    }
+  }
+
+  return (
+    <div className="vacationComponent">
+      {renderImage()}
+      <label className="name">{props.name}</label>
+      <br />
+      <label className="location">{props.location}</label>
+      <br />
+      <button onClick={() => props.reserveVacation(props.id, props.name)}>
+        Reserve
+      </button>
+    </div>
+  );
+}
