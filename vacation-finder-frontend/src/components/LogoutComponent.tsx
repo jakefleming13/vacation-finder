872775063c21d0d3aa0331c7b0d32b3ea30326a1
jakefleming13 <jakefleming@shaw.ca>
2025-06-07 12:47:37 -0600

    Create Logout component

---
+++ vacation-finder-frontend/src/components/LogoutComponent.tsx
@@ -0,0 +1,13 @@
+export default function LogoutComponent() {
+  return (
+    <div role="main">
+      <h2>Please login</h2>
+      <form>
+        <label>User name</label>
+        <br />
+        <input type="submit" value="Logout" />
+      </form>
+      <br />
+    </div>
+  );
+}
