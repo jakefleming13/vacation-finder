c943555a8ec9e1b88f835c992488bc0866896b89
jakefleming13 <jakefleming@shaw.ca>
2025-06-07 16:43:40 -0600

    Add tailwind to project

---
+++ vacation-finder-frontend/postcss.config.js
@@ -0,0 +1,7 @@
+// postcss.config.js
+export default {
+  plugins: {
+    tailwindcss: {}, // This line tells PostCSS to use the Tailwind CSS plugin
+    autoprefixer: {}, // Autoprefixer is often included with Tailwind to add vendor prefixes
+  },
+};
